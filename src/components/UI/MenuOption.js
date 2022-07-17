07e2e21ee3583609af9d62b4f2eec77fde6b4d30
{237ganbaru365} <fuminayoshimura2@gmail.com>
2022-07-17 11:51:57 -0700

    add menu

---
+++ src/components/UI/MenuOption.js
@@ -0,0 +1,16 @@
+import React, { memo } from "react";
+import { Link } from "react-router-dom";
+
+export const MenuOption = memo(({ className, icon, content, to, onClick }) => {
+  const menuOption = (
+    <div className={className}>
+      <h1>{icon}</h1>
+      <h4>{content}</h4>
+    </div>
+  );
+  return to ? (
+    <Link to={to}>{menuOption}</Link>
+  ) : (
+    <div onClick={onClick}>{menuOption}</div>
+  );
+});
--- src/features/post/Posts.js
+++ src/features/post/Posts.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { Menu } from "../../components/UI/Menu";
+import { NoPosts } from "../../components/UI/NoPosts";
+import { Post } from "./Post";
 
 export const Posts = () => {
-  return <div>Posts</div>;
+  let content;
+
+  //FIXME: ステータスごとの条件分岐
+  // content = <NoPosts />;
+  content = (
+    <div className="FlexCenter my-5">
+      <Post />
+      <Post />
+    </div>
+  );
+
+  return (
+    <>
+      <Menu />
+      {content}
+    </>
+  );
 };
