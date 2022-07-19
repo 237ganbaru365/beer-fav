3cf9a94d9a89b55767557f13402d271dfad0bada
{237ganbaru365} <fuminayoshimura2@gmail.com>
2022-07-18 22:10:44 -0700

    fix conflict

--- src/features/post/Posts.js
+++ src/features/post/Posts.js
@@ -1,10 +1,28 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
+import { Menu } from "../../components/UI/Menu";
+import { NoPosts } from "../../components/UI/NoPosts";
+import { Post } from "./Post";
+
 export const Posts = () => {
   const navigate = useNavigate();
+
+  let content;
+
+  //FIXME: ステータスごとの条件分岐
+  // content = <NoPosts />;
+  content = (
+    <div className="FlexCenter my-5">
+      <Post />
+      <Post />
+    </div>
+  );
+
   return (
     <>
+      <Menu />
+      {content}
       <p>This is posts page</p>
       <button onClick={() => navigate("/new")}>Create new post</button>
     </>
