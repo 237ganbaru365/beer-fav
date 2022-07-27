c506429f0623e47bbd44e27b632daf1b40b49dbd
{237ganbaru365} <fuminayoshimura2@gmail.com>
2022-07-27 13:54:03 -0700

    add my posts list

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -27,12 +27,20 @@ export const userSlice = createSlice({
       state.auth = null;
       state.user = null;
     },
-    setUser: (state, action) => {
-      state.user = action.payload;
+    addMyPostId: (state, action) => {
+      const { myPostId } = action.payload;
+
+      if (state.user.myPostIdList && state.user.myPostIdList.length > 0) {
+        state.user.myPostIdList.push(myPostId);
+      } else {
+        const myPostIdList = [myPostId];
+        state.user.myPostIdList = myPostIdList;
+      }
     },
   },
 });
 
-export const { signup, login, logout, setUser } = userSlice.actions;
+export const { signup, login, logout, setUser, addMyPostId } =
+  userSlice.actions;
 
 export const userReducer = userSlice.reducer;
